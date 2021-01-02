0fd2017fcf4567790073dfaf3d36931362e7985b
Hillary Okello <hillariouskelly@outlook.com>
2021-01-03 00:23:13 +0300

    finish SUM and COUNT

--- zoo.sql
+++ zoo.sql
@@ -34,7 +34,7 @@ SELECT * FROM nobel WHERE winner = 'EUGENE O''NEILL';
 SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner ASC;
 SELECT winner, subject FROM nobel WHERE yr=1984 ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner;
 
---SELECT within SELECT
+--SELECT in SELECT
 SELECT name FROM world WHERE population > (SELECT population FROM world WHERE name='Russia');
 SELECT name FROM world WHERE continent = 'Europe' AND gdp / population > (SELECT gdp / population FROM world WHERE name = 'United Kingdom');
 SELECT name, continent FROM world
@@ -49,3 +49,13 @@ SELECT continent, name, area FROM world x WHERE area >= ALL (SELECT area FROM wo
 SELECT continent, name FROM world x WHERE name <= ALL (SELECT name FROM world y WHERE y.continent=x.continent);
 SELECT name, continent, population FROM world x WHERE 25000000 > ALL(SELECT population FROM world y WHERE y.continent=x.continent AND y.population > 0);
 SELECT name, continent FROM world x WHERE population > ALL(SELECT population * 3 FROM world y WHERE y.continent=x.continent AND y.population > 0 AND x.name != y.name);
+
+--SUM and COUNT
+SELECT SUM(population) FROM world;
+SELECT DISTINCT(continent) FROM world;
+SELECT SUM(gdp) FROM world WHERE continent = 'Africa';
+SELECT COUNT(area) FROM world WHERE area > 1000000;
+SELECT SUM(population) FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+SELECT continent, COUNT(name) FROM world GROUP BY continent;
+SELECT continent, COUNT(name) FROM world WHERE population > 10000000 GROUP BY continent;
+SELECT continent FROM world GROUP BY continent HAVING SUM(population) > 100000000;
