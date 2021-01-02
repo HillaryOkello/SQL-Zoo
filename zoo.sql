5dc720e003ffa899df3f7b2af23477896c00a2a9
Hillary Okello <hillariouskelly@outlook.com>
2021-01-03 00:16:37 +0300

    finish select from nobel

--- zoo.sql
+++ zoo.sql
@@ -17,3 +17,19 @@ SELECT name, ROUND(GDP / population / 1000, 0) * 1000 FROM world WHERE GDP > 100
 SELECT name, capital FROM world WHERE LENGTH(name) = LENGTH(capital);
 SELECT name, capital FROM world WHERE LEFT(name,1) = LEFT(capital,1) AND name != capital;
 SELECT name FROM world WHERE name LIKE '%a%' AND name LIKE '%e%'AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
+
+--SELECT FROM NOBEL
+SELECT yr, subject, winner FROM nobel WHERE yr = 1950;
+SELECT winner FROM nobel WHERE yr = 1962 AND subject = 'Literature';
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein';
+SELECT winner FROM nobel WHERE yr >= 2000 AND subject = 'Peace';
+SELECT yr, subject, winner FROM nobel WHERE yr >= 1980 AND yr <= 1989 AND subject = 'Literature';
+SELECT * FROM nobel WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
+SELECT winner FROM nobel WHERE winner LIKE 'John%';
+SELECT yr, subject, winner FROM nobel WHERE (yr = 1980 AND subject = 'Physics') OR (yr = 1984 AND subject = 'Chemistry');
+SELECT yr, subject, winner FROM nobel WHERE yr = 1980 AND subject NOT LIKE '%Chemistry%' AND subject NOT LIKE '%medicine%';
+SELECT yr, subject, winner FROM nobel WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004);
+SELECT * FROM nobel WHERE winner = 'PETER GRÜNBERG';
+SELECT * FROM nobel WHERE winner = 'EUGENE O''NEILL';
+SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner ASC;
+SELECT winner, subject FROM nobel WHERE yr=1984 ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner;
